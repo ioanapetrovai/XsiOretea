0a910a32b9443264bf9c7d4a3e80a5f84a431ccc
nicoletadumitru <nicoleta.luciana13@gmail.com>
2017-05-26 16:03:01 +0300

    a

--- XsiO/src/pack/TicTacToeClient.java
+++ XsiO/src/pack/TicTacToeClient.java
@@ -60,12 +60,7 @@ public class TicTacToeClient {
         frame.getContentPane().add(boardPanel, "Center");
     }
 
-   //* The main thread of the client will listen for messages from the server.  
-   //The first message will be a "WELCOME" message in which we receive our mark.  
-   //Then we go into a loop listening for: 
-   //--> "VALID_MOVE", --> "OPPONENT_MOVED", --> "VICTORY", --> "DEFEAT", --> "TIE", --> "OPPONENT_QUIT, --> "MESSAGE" messages, and handling each message appropriately.
-   //The "VICTORY","DEFEAT" and "TIE" ask the user whether or not to play another game. 
-   //If the answer is no, the loop is exited and the server is sent a "QUIT" message.  If an OPPONENT_QUIT message is recevied then the loop will exit and the server will be sent a "QUIT" message also.
+  
     public void play() throws Exception {
         String response;
         try {
